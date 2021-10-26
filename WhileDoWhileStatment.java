cb7036f65efea1ace3e4f101eca1bdafb6f73f7f
vishu-prajapati <vishup0052gmail.com>
2021-10-26 12:23:24 +0530

    while and do while loop

---
+++ WhileDoWhileStatment.java
@@ -0,0 +1,25 @@
+import java.lang.*;
+import java.util.*;
+
+public class WhileDoWhileStatment
+{
+	public static void main(String args[])
+	{
+		/*while loop*/
+		int number = 1;
+		while(number <= 10)
+		{
+			System.out.println(number);
+			number++;
+		}
+	
+		/*do while loop*/
+		int number1 = 1;
+		do
+		{
+			System.out.println(number1);
+			number1++;
+		}while(number1 <= 10);
+	
+	}
+}
