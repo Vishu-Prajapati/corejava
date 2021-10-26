293ef98f32eca9a372f5f8f8cfaa39b4f1db73b7
vishu-prajapati <vishup0052gmail.com>
2021-10-26 12:19:30 +0530

    constructor

---
+++ Constructor.java
@@ -0,0 +1,35 @@
+import java.lang.*;
+import java.util.*;
+
+public class Constructor
+{
+	/*Program using constructor and parameterized constructor*/
+	
+	int numberOne;
+	int numberTwo;
+	
+	Constructor()
+	{
+		System.out.println("This is default constructor call");
+	}
+	
+	Constructor(int numberOne1,int numberTwo2)
+	{
+		this.numberOne = numberOne1;
+		this.numberTwo = numberTwo2;
+		
+		System.out.println("This is parameterized constructor call");
+	}
+	
+	void calculate()
+	{
+		int addition = numberOne + numberTwo;
+		System.out.println("Addition is : "+addition);
+	}
+	
+	public static void main(String args[])
+	{
+		Constructor c=new Constructor(10,20); //If we dont pass any value in argument then default constructor will call
+		c.calculate();
+	}
+}
