9fdf5eda60546cffff6dc7214e5229601c22f7ca
vishu-prajapati <vishup0052gmail.com>
2021-10-26 12:23:50 +0530

    wrapper class

---
+++ WrapperClass.java
@@ -0,0 +1,36 @@
+import java.lang.*;
+import java.util.*;
+
+class WrapperClass
+{
+	public static void main(String args[])
+	{
+		/*Program to compare two variable using wrapper class*/
+		
+		int i = 10;
+		int i1 = 10;
+		
+		/*For int datatype wrapper class is Integer
+		  For float datatype wrapper class is Float
+		  For boolean datatype wrapper class is Boolean
+		  .....
+		  .....
+		*/
+		
+		Integer intobj = i;
+		System.out.println("Interger object : "+intobj);
+		
+		Integer intobj1 = i1;
+		System.out.println("Interger object : "+intobj1);
+		
+		if(intobj == intobj1)
+		{
+			System.out.println("its value is same...");
+		}
+		else
+		{
+			System.out.println("its value is not same...");
+		}
+	}
+	
+}
