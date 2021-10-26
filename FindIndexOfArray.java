2a3124ddb83ac83f340556707d5decd5dc4d27bc
Vishu-Prajapati <93139713+Vishu-Prajapati@users.noreply.github.com>
2021-10-26 18:59:13 +0530

    Find index of an array element

---
+++ FindIndexOfArray.java
@@ -0,0 +1,45 @@
+/*Program to find index of an array element*/
+
+import java.lang.*;
+import java.util.*;
+
+public class FindIndexOfArray
+{
+	public static void main(String args[])
+	{
+		int[] array = new int[10];
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Enter how many elements you want : ");
+		int number = scanner.nextInt();
+		
+		for(int i = 0 ; i < number ; i++)
+		{
+			array[i] = scanner.nextInt();
+		}
+		
+		System.out.println("Array Elements are : ");
+		
+		for(int i = 0 ; i < number ; i++)
+		{
+			System.out.println(array[i]);
+		}
+		
+		System.out.println("Enter an element which you want index of that element : ");
+		
+		int element = scanner.nextInt();
+		int index = -1;
+		
+		for(int i = 0 ; i < number ; i++)
+		{
+			if(array[i] == element)
+			{
+				index = i;
+				break;
+			}
+		}
+		
+		System.out.println("Index number is : "+index);
+	}
+}
