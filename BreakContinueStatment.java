4396646f1fbfbcb45520a65042658c4fc268ad24
Vishu-Prajapati <93139713+Vishu-Prajapati@users.noreply.github.com>
2021-10-27 10:41:59 +0530

    update break and continue statment

--- BreakContinueStatment.java
+++ BreakContinueStatment.java
@@ -2,17 +2,32 @@ public class BreakContinueStatment
 {
 	public static void main(String args[])
 	{
-		/*Program using Break and Continue statments*/
+		/*Program using Break statment*/
 		
-		int number = 1;
+		System.out.println("===Break Statment===");
 		
-		for(number =0 ; number <= 10 ; number++)
+		for(int numberOne = 0 ; numberOne <= 10 ; numberOne++)
 		{
-			if (number == 7)
+			if (numberOne == 7)
 			{
-				continue; //If we use break statment then loop terminate after 6 number
+				break; //If we use break statment then loop terminate after 6 number
 			}
-			System.out.println(number);
-		}		
+			System.out.println(numberOne);
+		}
+
+		System.out.println();
+		
+		/*Program using Continue statment*/
+		
+		System.out.println("===Continue Statment===");
+		
+		for(int numberTwo = 0 ; numberTwo <= 10 ; numberTwo++)
+		{
+			if (numberTwo == 4)
+			{
+				continue; //If we use continue statment then loop then it skips 4 and then continue 
+			}
+			System.out.println(numberTwo);
+		}
 	}
 }
