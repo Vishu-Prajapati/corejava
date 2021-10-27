ca7de92ab35f72e937b3b6d974f89c7f1b4629b6
Vishu-Prajapati <93139713+Vishu-Prajapati@users.noreply.github.com>
2021-10-27 15:09:09 +0530

    Update static method file

--- StaticMethod.java
+++ StaticMethod.java
@@ -1,13 +1,23 @@
 /*Program using static methods*/
 
+import java.util.Scanner;
+
 class Test
 {
-	static int numberOne = 10;
-	static int numberTwo = 20;
+	static int numberOne;
+	static int numberTwo;
 	static int addition;
 		
 	static void Display() //Static method accesses only static variables
 	{
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter your first number : ");
+		numberOne = scanner.nextInt();
+		
+		System.out.println("Enter your second number : ");
+		numberTwo = scanner.nextInt();
+		
 		addition = numberOne + numberTwo;
 		System.out.println("Answer is : "+addition);
 	}
