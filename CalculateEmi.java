f72ac4fec96173c03f74948f8a3d0fa13e6463ce
Vishu-Prajapati <93139713+Vishu-Prajapati@users.noreply.github.com>
2021-10-27 14:51:41 +0530

    Update calculate emi file

--- CalculateEmi.java
+++ CalculateEmi.java
@@ -1,32 +1,38 @@
-import java.util.Scanner;
-
-class CalculateEmi
+public class CalculateEmi
 {
 	/*Program to Calculate EMI*/
 	
-	void Calculate()
+	double amount;
+	double interest;
+	double time;
+	
+	CalculateEmi()
 	{
-		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Enter your amount : ");
-		float amount = sc.nextFloat();
-		
-		System.out.println("Enter your interest : ");
-		float interest = sc.nextFloat();
-
-		System.out.println("Enter your time in year : ");
-		float time = sc.nextFloat();
-		
+	}
+	
+	CalculateEmi(double amount, double interest, double time)
+	{
+		this.amount = amount;
+		this.interest = interest;
+		this.time = time;
+	}
+	
+	void CalculateEmiDisplay()
+	{
 		interest = interest / ( 12 * 100 ); 
-       	        time = time * 12; 
+        time = time * 12; 
       
 		double emi = ( amount * interest * Math.pow ( 1 + interest , time ) ) / ( Math.pow ( 1 + interest , time ) - 1);
 
 		System.out.println("Calculated Emi is : "+emi);
 	}
+	
 	public static void main(String args[])
 	{
-		CalculateEmi calculateEmi=new CalculateEmi();
-		calculateEmi.Calculate();
+		CalculateEmi calculateEmi = new CalculateEmi(2000.00,20,2);
+		calculateEmi.CalculateEmiDisplay();
 	}
 }
+		
+	
